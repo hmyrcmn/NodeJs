6ffdc60a9bcec4411f58a6aae600f70b93b0767a
HÜMEYRA ÇİMEN <humeyracimen2001@gmail.com>
2024-03-24 14:15:28 +0300

    update

--- week5/try.js
+++ week5/try.js
@@ -1,17 +1,11 @@
-const request=require('postman-request')
+const request = require('request');
+const url = 'http://api.weatherstack.com/current?access_key=YOUR_ACCESS_KEY&query=37.8267,-122.4233';
 
-const url="http://api.weatherstack.com/current?access_key=5c3376fd0dd61382d80dc06521e4bf2a&query=37.8267,-122.4233&units=f"
-request({url:url,json:true},(error,response)=> {
-        console.log(error)
-        if(error){
-                //wifi kapattım 
-                console.log("hava durumu servisine bağlantı kurulamadı ! ")
-        }else{
-        // const data=JSON.parse(response.body)
-        // console.log(data.current)
-
-        console.log("hava sıcaklıgı :",response.body.current.temperature+
-        " \n hissedilen sıcaklık: ",response.body.current.feelslike)
-        }
-   
-})
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        console.log('Hata:', error);
+    } else {
+        console.log('Hava Sıcaklığı:', response.body.current.temperature);
+        console.log('Hissedilen Sıcaklık:', response.body.current.feelslike);
+    }
+});
